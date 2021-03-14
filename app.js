efcebd4802821510f546cdec881fef1d39d8980c
jaime <jaime@MacBook-Pro.local>
2021-03-14 14:56:42 -0600

    added validation to answers

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username (Required)',
+      validate: usernameInput => {
+        if (usernameInput) {
+          return true;
+        } else {
+          console.log('Please enter your username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -50,7 +66,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project description!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -61,7 +85,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('You need to enter a project GitHub link!');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
